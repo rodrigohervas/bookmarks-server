8cfe4e42f70c515e735832dd54503574e95ec6f5
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-03-21 12:41:46 -0400

    Modified conn from DB_URL to DATABASE_URL

--- src/knexContext.js
+++ src/knexContext.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 
 const db = knex({
     client: 'pg', 
-    connection: (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : process.env.DB_URL
+    connection: (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : process.env.DATABASE_URL
 })
 
 module.exports = db
